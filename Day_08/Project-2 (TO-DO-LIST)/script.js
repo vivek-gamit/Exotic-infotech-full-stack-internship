011245895696eda6b8088b99c1bf77e7085ad08e
vivek-gamit <vgamit637@gmail.com>
2026-01-12 15:08:10 +0530

    To-Do-List

--- Day_08/Project-2 (TO-DO-LIST)/script.js
+++ Day_08/Project-2 (TO-DO-LIST)/script.js
@@ -1,37 +1,64 @@
 let add = document.getElementById('add');
 
-function update(){
- 
-
+function getAndUpdate() {
     console.log('Updating list...');
+
     let tit = document.getElementById('titInput').value;
     let dec = document.getElementById('Description').value;
 
-    if (localStorage.getItem('itemsJson') === null) {
-        itemJsonarray = []
-        itemJsonarray.push([tit, dec])
-        localStorage.setItem('itemsJson', JSON.stringify(itemJsonarray))
-    } else {
-        itemJsonarrayStr = localStorage.getItem('itemsJson');
-        itemJsonarray = JSON.parse(itemJsonarrayStr)
-        itemJsonarray.push([tit, dec])
-        localStorage.setItem('itemsJson', JSON.stringify(itemJsonarray))
+   
+    if (tit.trim() === "" || dec.trim() === "") {
+        alert("Please enter both title and description");
+        return;
     }
 
+    let itemJsonarray;
+
+    if (localStorage.getItem('itemsJson') === null) {
+        itemJsonarray = [];
+    } else {
+        itemJsonarray = JSON.parse(localStorage.getItem('itemsJson'));
+    }
+
+    itemJsonarray.push([tit, dec]);
+    localStorage.setItem('itemsJson', JSON.stringify(itemJsonarray));
+
+
+    document.getElementById('titInput').value = "";
+    document.getElementById('Description').value = "";
+
+    updateTable();
+}
+
+function updateTable() {
     let tablebody = document.getElementById('tablebody');
+    let itemJsonarray = JSON.parse(localStorage.getItem('itemsJson')) || [];
     let str = "";
-    itemJsonarray.forEach((Element, index) => {
-        str += `                        
+
+    itemJsonarray.forEach((element, index) => {
+        str += `
         <tr>
             <th scope="row">${index + 1}</th>
-            <td>${Element[0]}</td>
-            <td>${Element[1]}</td>
-            <td><button class="btn btn-sm btn-primary">Delete</button></td>
-                            
-        </tr>`
+            <td>${element[0]}</td>
+            <td>${element[1]}</td>
+            <td>
+                <button class="btn btn-sm btn-danger" onclick="deleteItem(${index})">
+                    Delete
+                </button>
+            </td>
+        </tr>`;
     });
+
     tablebody.innerHTML = str;
 }
 
-add.addEventListener('click', update);
-update();
+function deleteItem(index) {
+    let itemJsonarray = JSON.parse(localStorage.getItem('itemsJson'));
+    itemJsonarray.splice(index, 1);
+    localStorage.setItem('itemsJson', JSON.stringify(itemJsonarray));
+    updateTable();
+}
+
+
+add.addEventListener('click', getAndUpdate);
+updateTable();
