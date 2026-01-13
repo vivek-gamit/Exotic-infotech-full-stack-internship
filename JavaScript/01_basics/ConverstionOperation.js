d34b1fc90c80b5b47323f52d34fcd57884c51c56
vivek-gamit <vgamit637@gmail.com>
2026-01-13 15:44:32 +0530

    TypeConvertion

---
+++ JavaScript/01_basics/ConverstionOperation.js
@@ -0,0 +1,14 @@
+let string = 'vivek'
+
+console.log(string);
+
+let conStr = Number(string)
+
+console.log(conStr);
+console.log(typeof(conStr));
+
+
+let BooleanStr = Boolean(string)
+console.log(BooleanStr);
+console.log(typeof(BooleanStr));
+
