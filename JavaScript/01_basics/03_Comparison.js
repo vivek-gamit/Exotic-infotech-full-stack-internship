41c636788d7ab461e34bbcb24ac728eb328f57a6
vivek-gamit <vgamit637@gmail.com>
2026-01-13 16:17:21 +0530

    Comparison of null and undefined

---
+++ JavaScript/01_basics/03_Comparison.js
@@ -0,0 +1,17 @@
+console.log(null == 0);
+console.log(null > 0);
+console.log(null < 0);
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+console.log(null == undefined);
+console.log(null > undefined);
+console.log(null < undefined);
+
+
+
+
+
+
