bbff8deea2a90294ad18246371ebc6fa6cc1c22f
vivek-gamit <vgamit637@gmail.com>
2026-01-16 15:42:09 +0530

    Date_and_time

---
+++ JavaScript/01_basics/Date_and_time.js
@@ -0,0 +1,34 @@
+let mydate = new Date();
+
+// console.log(mydate);
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleString());
+// console.log(`Today date is ${mydate.getDate()} and month is ${mydate.getMonth() + 1} and year is ${mydate.getFullYear()}`);
+
+
+// let mycreatedDate = new Date(2028,0,12);
+// let mycreatedDate = new Date(2026,0,16,15,34,32);
+// let mycreatedDate = new Date("2026-01-16");
+// let mycreatedDate = new Date("01-23-2026");
+// console.log(mycreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let getDate = new Date();
+
+// console.log(getDate);
+// console.log(getDate.getDate());
+console.log(getDate.getMonth()+1);
+
+
+
+
+
+
+
