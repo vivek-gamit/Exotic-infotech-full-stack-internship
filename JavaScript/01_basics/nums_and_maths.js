5b6641c27a583ad7b2c961d2756ee4af78555b99
vivek-gamit <vgamit637@gmail.com>
2026-01-16 15:20:13 +0530

    Nums_and_Maths

---
+++ JavaScript/01_basics/nums_and_maths.js
@@ -0,0 +1,36 @@
+let score = 400;
+
+// console.log(score);
+
+let balance = new Number(100);
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+let otherNumber = 1123.79358;
+// console.log(otherNumber.toPrecision(5));
+
+const hundreds = 10000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-45));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(34.6));
+// console.log(Math.ceil(34.4));
+
+let min = 10;
+let max = 20;
+
+console.log(Math.floor(Math.random()*10) + min);
+
+
+
+
+
+
+
