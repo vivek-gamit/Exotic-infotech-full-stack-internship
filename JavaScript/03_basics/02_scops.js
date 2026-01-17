37aff2a477a12c1cd895df3dc91b77947d90dd41
vivek-gamit <vgamit637@gmail.com>
2026-01-17 16:37:47 +0530

    Inner function in JS

--- JavaScript/03_basics/02_scops.js
+++ JavaScript/03_basics/02_scops.js
@@ -3,12 +3,29 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
     
 }
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+/* ++++++++++++++++++ inner function ++++++++++++++++*/
+
+function one(){
+    const userName ='vivek';
+
+    function two(){
+        console.log(`Hello my name is ${userName}`);
+        const website = 'E-learning'
+    }
+
+    // console.log(website);
+
+    two()
+}
+
+one()
